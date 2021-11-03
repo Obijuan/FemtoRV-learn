e8aa791afad40b858347e37390896c1d29037e9f
obijuan <obijuan.cube@gmail.com>
2021-11-03 23:52:44 +0100

    Uart moved to a separated file

--- FemtoRV-quark-APIO/femtosoc.v
+++ FemtoRV-quark-APIO/femtosoc.v
@@ -45,56 +45,6 @@
 `define NRV_ABI      "ilp32"
 `define NRV_OPTIMIZE "-Os"
 
-
-module UART(
-    input wire 	       clk,      // system clock
-    input wire 	       rstrb,    // read strobe		
-    input wire 	       wstrb,    // write strobe
-    input wire 	       sel_dat,  // select data reg (rw)
-    input wire 	       sel_cntl, // select control reg (r) 	       	    
-    input wire [31:0]  wdata,    // data to be written
-    output wire [31:0] rdata,    // data read
-
-    input wire 	       RXD, // UART pins
-    output wire        TXD,
-
-    output reg         brk  // goes high one cycle when <ctrl><C> is pressed. 	    
-);
-
-wire [7:0] rx_data;
-wire [7:0] tx_data;
-wire serial_tx_busy;
-wire serial_valid;
-
-buart #(
-  .FREQ_MHZ(`NRV_FREQ),
-  .BAUDS(115200)
-) the_buart (
-   .clk(clk),
-   .resetq(!brk),
-   .tx(TXD),
-   .rx(RXD),
-   .tx_data(wdata[7:0]),
-   .rx_data(rx_data),
-   .busy(serial_tx_busy),
-   .valid(serial_valid),
-   .wr(sel_dat && wstrb),
-   .rd(sel_dat && rstrb) 
-);
-
-assign rdata =   sel_dat  ? {22'b0, serial_tx_busy, serial_valid, rx_data} 
-               : sel_cntl ? {22'b0, serial_tx_busy, serial_valid, 8'b0   } 
-               : 32'b0;   
-
-always @(posedge clk) begin
-   brk <= serial_valid && (rx_data == 8'd3);
-end
-
-endmodule
-
-
-
-
 `ifdef ICE_STICK
  `define SPI_FLASH_FAST_READ_DUAL_IO
  `define SPI_FLASH_CONFIGURED
